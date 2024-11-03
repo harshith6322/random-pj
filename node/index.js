9fac3428308a4464e7a61e41f30bdb5311906f87
harshithreddy6322 <harshithreddy6322@gmail.com>
2024-11-03 15:41:41 +0530

    updated

--- node/index.js
+++ node/index.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const fs = require("node:fs");
 const os = require("node:os");
+const process = require("node:process");
+const cluster = require("node:cluster");
 
 const result = os.cpus().length;
 const app = express();
 app.use(require("express-status-monitor")());
+let count = 0;
 
-app.get("/", async (req, res) => {
-  
-});
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000/");
-});
+  // Fork workers.
+  for (let i = 0; i < result; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    // cluster.fork();
+  });
+} else {
+  app.get("/", async (req, res) => {
+    let val = count++;
+
+    console.log(`id : ${process.pid}`);
+    res.json({
+      result,
+      health: "good",
+      val: val * 2 + 1,
+      id: process.pid,
+    });
+  });
+
+  app.listen(3000, () => {
+    console.log("http://localhost:3000/");
+  });
+}
